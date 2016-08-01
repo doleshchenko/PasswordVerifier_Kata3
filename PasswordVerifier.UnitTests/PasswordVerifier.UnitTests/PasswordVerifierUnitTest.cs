77b122d4f2bb62c41f3ee9f8451d64ac95882595
Dmytro Oleshchenko <dima.oleshchenko@outlook.com>
2016-08-02 00:03:59 +0200

    First five steps of kata 3

---
+++ PasswordVerifier.UnitTests/PasswordVerifier.UnitTests/PasswordVerifierUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordVerifier.UnitTests
+{
+    [TestClass]
+    public class PasswordVerifierUnitTest
+    {
+        [TestMethod]
+        public void Verify_StringLessThan8CharactersAsInputParam_ReturnsFalse()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify("1234567");
+            Assert.AreEqual(false, result, "Method should have returned false for '1234567' string.");
+        }
+
+        [TestMethod]
+        public void Verify_StringLongerThan8CharactersAsInputParam_ReturnsTrue()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify("1234567lU");
+            Assert.AreEqual(true, result, "Method should have returned true for '1234567lU' string.");
+        }
+
+        [TestMethod]
+        public void Verify_StringWithLengthEqualsTo8CharactersAsInputParam_ReturnsTrue()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify("123456lU");
+            Assert.AreEqual(true, result, "Method should have returned true for '123456lU' string.");
+        }
+
+        [TestMethod]
+        public void Verify_NullAsInputParam_ReturnsFalse()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify(null);
+            Assert.AreEqual(false, result, "Method should have returned false for null.");
+        }
+
+        [TestMethod]
+        public void Verify_8SymbolsStringWithoutSymbolsInUpperCaseAsInputParam_ReturnsFalse()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify("12345678");
+            Assert.AreEqual(false, result, "Method should have returned false for '12345678' string..");
+        }
+
+        [TestMethod]
+        public void Verify_8SymbolsStringWithoutSymbolsInLowerCaseAsInputParam_ReturnsFalse()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify("1234567U");
+            Assert.AreEqual(false, result, "Method should have returned false for '1234567U' string..");
+        }
+
+        [TestMethod]
+        public void Verify_8SymbolsStringWithoutNumbersAsInputParam_ReturnsFalse()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var result = passwordVerifier.Verify("abcdrtdU");
+            Assert.AreEqual(false, result, "Method should have returned false for 'abcdrtdU' string..");
+        }
+    }
+}
---
+++ PasswordVerifier.UnitTests/PasswordVerifier/PasswordVerifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVerifier
+{
+    public class PasswordVerifier
+    {
+        public bool Verify(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            if (!password.Any(char.IsUpper))
+            {
+                return false;
+            }
+            if (!password.Any(char.IsLower))
+            {
+                return false;
+            }
+            if (!password.Any(char.IsNumber))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
